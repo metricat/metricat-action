2b7761ecc0a8d7a26ecf6b57b934afe0de288f19
Wei Zhu <yesmeck@gmail.com>
2023-11-23 17:28:54 +1030

    skip test

--- dist/index.js
+++ dist/index.js
@@ -2753,22 +2753,28 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.run = void 0;
 const core = __importStar(__nccwpck_require__(186));
-const wait_1 = __nccwpck_require__(259);
+const fs_1 = __nccwpck_require__(147);
+const promises_1 = __nccwpck_require__(292);
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
     try {
-        const ms = core.getInput('milliseconds');
-        // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-        core.debug(`Waiting ${ms} milliseconds ...`);
-        // Log the current timestamp, wait, then log the new timestamp
-        core.debug(new Date().toTimeString());
-        await (0, wait_1.wait)(parseInt(ms, 10));
-        core.debug(new Date().toTimeString());
-        // Set outputs for other workflow steps to use
-        core.setOutput('time', new Date().toTimeString());
+        if (!(0, fs_1.existsSync)('codeyet.json')) {
+            console.log('codeyet.json not found');
+            return;
+        }
+        const metrics = JSON.parse(await (0, promises_1.readFile)('codeyet.json', 'utf8'));
+        const api = core.getInput('api') ?? 'https://codeyet.dev/api/v1/metrics';
+        await fetch(api, {
+            method: 'POST',
+            body: JSON.stringify({
+                token: core.getInput('token'),
+                sha: process.env.GITHUB_SHA,
+                metrics
+            })
+        });
     }
     catch (error) {
         // Fail the workflow run if an error occurs
@@ -2777,31 +2783,6 @@ async function run() {
     }
 }
 exports.run = run;
-
-
-/***/ }),
-
-/***/ 259:
-/***/ ((__unused_webpack_module, exports) => {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.wait = void 0;
-/**
- * Wait for a number of milliseconds.
- * @param milliseconds The number of milliseconds to wait.
- * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
- */
-async function wait(milliseconds) {
-    return new Promise(resolve => {
-        if (isNaN(milliseconds)) {
-            throw new Error('milliseconds not a number');
-        }
-        setTimeout(() => resolve('done!'), milliseconds);
-    });
-}
-exports.wait = wait;
 
 
 /***/ }),
@@ -2835,6 +2816,14 @@ module.exports = require("events");
 
 "use strict";
 module.exports = require("fs");
+
+/***/ }),
+
+/***/ 292:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("fs/promises");
 
 /***/ }),
 
