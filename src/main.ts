9c20f486d1b3913605d21493e163ee17533e9bb5
Wei Zhu <yesmeck@gmail.com>
2023-12-14 18:44:43 +1030

    add run id

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,19 @@ import * as core from '@actions/core'
 import { existsSync } from 'fs'
 import { readFile } from 'fs/promises'
 
+const METRICS_FILE = "metrics.json"
+
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
   try {
-    if (!existsSync('metricat.json')) {
-      console.log('metricat.json not found')
+    if (!existsSync(METRICS_FILE)) {
+      console.log(`${METRICS_FILE} not found`)
       return
     }
-    const metrics = JSON.parse(await readFile('metricat.json', 'utf8'))
+    const metrics = JSON.parse(await readFile(METRICS_FILE, 'utf8'))
 
     const api = core.getInput('api') ?? 'https://metricat.dev/api/v1/metrics'
 
@@ -25,6 +27,7 @@ export async function run(): Promise<void> {
         token: core.getInput('token'),
         commit: {
           sha: process.env.GITHUB_SHA,
+          runId: process.env.GITHUB_RUN_ID,
           refName: process.env.GITHUB_REF_NAME,
           headRef: process.env.GITHUB_HEAD_REF,
           baseRef: process.env.GITHUB_BASE_REF
